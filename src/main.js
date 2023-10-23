a776111e471133e09c8e843708a94a517e5b47e6
Agung Aji Pradana <agungajipradana@gmail.com>
2023-10-24 05:33:19 +0700

    add cors

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
 
-web.listen(3000, () => {
-  logger.info("App start");
+// web.listen(3002, () => {
+//   logger.info("App start");
+// });
+
+const port = process.env.PORT || 3002;
+web.listen(3002, () => {
+  logger.info(`Server listening on port ${port}`);
 });
+
+// const port = process.env.PORT;
+// app.listen(port, () => console.log(`Server listening on port ${port}`));
